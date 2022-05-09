052bf8eb6234a77e4c2a16ef1cc8f2378c264516
andrecouto <andrecouto.jc.95@gmail.com>
2022-05-09 15:13:28 -0300

    Check logic.

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,8 +12,12 @@ public abstract class ChessPiece extends Piece {
         this.color = color;
     }
 
-    protected boolean isThereOpponentPiece(Position position){
-        ChessPiece p =(ChessPiece) getBoard().piece(position);
+    public ChessPosition getChessPosition() {
+        return ChessPosition.fromPosition(position);
+    }
+
+    protected boolean isThereOpponentPiece(Position position) {
+        ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
     }
 
